802d450dc50f36a17fb1e8966f130e0090d75345
ses8516 <ses8516@rit.edu>
2013-10-30 13:11:01 -0400

    Update CGrep.java
    Initial input checking

--- Concurrent-Grep-With-Actors/src/CGrep.java
+++ Concurrent-Grep-With-Actors/src/CGrep.java
@@ -6,7 +6,16 @@ public class CGrep {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		if(args.length == 0){
+			System.err.println("No Pattern Inputted. Exiting...");
+			System.exit(0);
+		}
+		String pattern = args[0];
+		if(args.length == 1){
+			// Standard Input will be used
+		}else{
+			// Files will be used. Files are the rest of the elemetns in args
+		}
 	}
 
 }
